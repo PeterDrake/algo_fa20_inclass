dccaf5d169e2c531b8488dc4d6c2b4ca704ea806
Peter Drake <drake@lclark.edu>
2020-09-11 12:45:41 -0700

    Stacks, queues, and sets.

---
+++ src/inclass/Set.java
@@ -0,0 +1,13 @@
+package inclass;
+
+public interface Set<K> {
+
+    public void add(K key);
+
+    public boolean contains(K key);
+
+    public boolean isEmpty();
+
+    public void remove(K key);
+
+}
---
+++ src/inclass/Stack.java
@@ -0,0 +1,11 @@
+package inclass;
+
+public interface Stack<K> {
+
+    public boolean isEmpty();
+
+    public K pop();
+
+    public void push(K key);
+
+}
